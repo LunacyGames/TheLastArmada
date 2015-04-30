e7b9292e44710f15b26a44f3c5e08af899d59b63
chitzen <ledzeppfann@aol.com>
2015-04-29 21:16:04 -0400

    Added comments to most player classes. Check for TODOs

--- app/src/main/java/com/lunacygames/thelastarmada/player/Inventory.java
+++ app/src/main/java/com/lunacygames/thelastarmada/player/Inventory.java
@@ -30,9 +30,10 @@ public class Inventory {
     }
 
     /**
+     * Return Item effect
      *
-     * @param itemID
-     * @return
+     * @param itemID    ID of item
+     * @return          Item effect if itemID valid, null if not
      */
     public static String getItemEffect(int itemID) {
         switch(itemID) {
@@ -50,29 +51,53 @@ public class Inventory {
 
     /**
      * Initializes the player's inventory.
+     *
      */
     public static void initInventory() {
         inventory = new Integer[MAX_ITEM_NUMBER];
     }
 
+    /**
+     * Set item count of given itemID
+     * @param itemID        id of item
+     * @param itemCount     quantity of item
+     */
     public static void setItemCount(int itemID, int itemCount) {
         inventory[itemID] = itemCount;
     }
 
+    /**
+     * Increment the given itemID's quantity
+     * @param itemID        id of item
+     * @param itemCount     quantity of item
+     */
     public static void incrementItemCount(int itemID, int itemCount) {
         inventory[itemID] += itemCount;
     }
 
+    /**
+     * @deprecated by decrementItemCount
+     * @param itemID        id of item
+     */
     public static void useItem(int itemID) {
         if(inventory[itemID] == 0) return;
         else inventory[itemID]--;
     }
 
+    /**
+     * Decrement number of itemIDs in inventory
+     * @param itemID        id of item
+     */
     public static void decrementItemCount(int itemID) {
         if(inventory == null) return;
         else if(inventory[itemID] > 0) inventory[itemID]--;
     }
 
+    /**
+     * Get number of item specified by itemID
+     * @param itemID        id of item
+     * @return              quantity of itemID in inventory
+     */
     public static int getItemCount(int itemID) {
         return inventory[itemID];
     }
